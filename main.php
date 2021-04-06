5b97e4e1fd6f0fef3e34ea262f4c510286804e65
aziza810 <81962842+aziza810@users.noreply.github.com>
2021-04-06 12:09:27 +0600

    Create main.php

---
+++ main.php
@@ -0,0 +1,46 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="div">
+        <form  method="get">
+            <label for="login">Username</label>
+            <input type="text" id="fname" name="fname" ><br><br>
+            <label for="password" >Password</label>
+            <input  id="lname" type="password" name="lname"><br><br>
+            <button onclick="myFunction()">login</button>
+            <a href="https://instagram.com/" id="a"></a>
+
+          </form>
+          
+
+          
+    </div>
+    <h1 id="demo"></h1>
+    <script> 
+      function myFunction(){
+var x = document.getElementById('fname').value
+var b = document.getElementById('lname').value
+var form = document.querySelector('form')
+
+document.getElementById("demo").innerHTML = x;
+
+
+
+if (x == "aziza" , b == "12345"){
+    form.setAttribute("action", "https://instagram.com/")
+
+}else{
+    alert("permission denied")
+}
+
+}
+    </script>
+</body>
+</html> -->
