592a0a018ad53994b08d1cd968a9f093ce47918d
aziza810 <81962842+aziza810@users.noreply.github.com>
2021-04-06 11:54:09 +0600

    Update index.php

--- index.php
+++ index.php
@@ -33,17 +33,63 @@ h2{
 <h3>Web developer from Bishkek!</h3>
 <h3>Name: Aziza Dzhamangulova</h3>
 <h3>Age: 19</h3>
-<p>.</p>
-<p>.</p>
-<p>.</p>
-<p>.</p>
-<p>.</p>
-<p>.</p>
-<p>.</p>
-<p>.</p>
+
 <h3>Me in social media</h3>
 <a href="#" class="fa fa-youtube"></a>
 <a href="#" class="fa fa-instagram"></a>
 </body>
 </html>
 </br>
+<section class='all'>
+  <div class='float-content'>
+    <div class='content-logo'>
+      <h1>Painel Admin</h1>
+      <h4>Sing In</h4>
+      <span>
+        <i class='fa fa-fw fa-cog'></i>
+      </span>
+    </div>
+  </div>
+  <div class='content'>
+    <form class='form formLogin'>
+      <h1>
+        <i class='fa fa-fw fa-sign-in'></i>
+        Sign In
+      </h1>
+      <div class='group'>
+        <input placeholder='Username' type='text'>
+        <i class='fa fa-fw fa-user'></i>
+      </div>
+      <div class='group'>
+        <input placeholder='Password' type='password'>
+        <i class='fa fa-fw fa-key'></i>
+      </div>
+      <div class='group'>
+        <button type='submit'>Sign In</button>
+        <button id='singup'>Sing Up</button>
+      </div>
+    </form>
+    <form class='form formSingup'>
+      <h1>
+        <i class='fa fa-fw fa-user-plus'></i>
+        Sing Up
+      </h1>
+      <div class='group'>
+        <input placeholder='Username' type='text'>
+        <i class='fa fa-fw fa-user'></i>
+      </div>
+      <div class='group'>
+        <input placeholder='E-mail' type='email'>
+        <i class='fa fa-fw fa-envelope-o'></i>
+      </div>
+      <div class='group'>
+        <input placeholder='Password' type='password'>
+        <i class='fa fa-fw fa-key'></i>
+      </div>
+      <div class='group'>
+        <button id='singin'>Sign In</button>
+        <button type='submit'>Sing Up</button>
+      </div>
+    </form>
+  </div>
+</section>
