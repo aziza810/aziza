2068615730edb28f272d19a1a94e651f83773d99
aziza810 <81962842+aziza810@users.noreply.github.com>
2021-04-06 12:11:15 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,92 +1,46 @@
-
-<!DOCTYPE html>
-<html style="background: url(https://cdn.hipwallpaper.com/i/91/89/RK4kPX.png);"> 
+<!-- <!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <title></title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<style>
-  .fa {
-  padding: 20px;
-  font-size: 30px;
-  width: 50px;
-  text-align: center;
-  text-decoration: none;
-  margin: 5px 2px;
-}
-.fa-youtube {
-  background: #bb0000;
-  color: white;
-}
-.fa-instagram {
-  background: #125688;
-  color: white;
-}
-h2{
-  color: black;
-}
-}
-</style>
 <body>
-<h2><center>THIS IS MY PORTFOLIO!</center></h2>
-<h3>Web developer from Bishkek!</h3>
-<h3>Name: Aziza Dzhamangulova</h3>
-<h3>Age: 19</h3>
+    <div class="div">
+        <form  method="get">
+            <label for="login">Username</label>
+            <input type="text" id="fname" name="fname" ><br><br>
+            <label for="password" >Password</label>
+            <input  id="lname" type="password" name="lname"><br><br>
+            <button onclick="myFunction()">login</button>
+            <a href="https://instagram.com/" id="a"></a>
 
-<h3>Me in social media</h3>
-<a href="#" class="fa fa-youtube"></a>
-<a href="#" class="fa fa-instagram"></a>
-</body>
-</html>
-</br>
-<body>
-  <div class="content-wrapper">
-    <div class="background background-left"></div>
-    <div class="background background-right"></div>
-    <div class="sing-up-panel active">
-      <form>
-        <h1>Lets get started</h1>
-        <div class="input">
-          <input type="text" required id="name">
-          <div class="bar"></div>
-          <label>Username</label>
-        </div>
-        <div class="input">
-          <input type="text" required id="mail">
-          <div class="bar"></div>
-          <label>Mail</label>
-        </div>
-        <div class="input">
-          <input type="password" required id="password">
-          <div class="bar"></div>
-          <label>Password</label>
-        </div>
-        <div class="button-wrapper">
-          <button class="button-transparent sing-in" type="submit">Sing In</button>
-          <button class="sing-up" type="submit">Sing Up</button>
-        </div>
-      </form>
+          </form>
+          
+
+          
     </div>
-    <div class="sing-in-panel">
-      <form>
-        <h1>Sign In to your Dashboard</h1>
-        <div class="input">
-          <input type="text" required id="name">
-          <div class="bar"></div>
-          <label>Username</label>
-        </div>
-        <div class="input">
-          <input type="password" required id="password">
-          <div class="bar"></div>
-          <label>Password</label>
-        </div>
-        <div class="button-wrapper">
-          <button class="sing-in" type="submit">Sing In</button>
-          <button class="button-transparent sing-up" type="submit">Sing Up</button>
-        </div>
-      </form>
-    </div>
-    <div class="floating-content"></div>
-  </div>
+    <h1 id="demo"></h1>
+    <script> 
+      function myFunction(){
+var x = document.getElementById('fname').value
+var b = document.getElementById('lname').value
+var form = document.querySelector('form')
+
+document.getElementById("demo").innerHTML = x;
+
+
+
+if (x == "aziza" , b == "12345"){
+    form.setAttribute("action", "https://instagram.com/")
+
+}else{
+    alert("permission denied")
+}
+
+}
+    </script>
 </body>
+</html> -->
